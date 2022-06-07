826c0e2661e6aa84bb900cbd1ed6d0295d4c9b6f
jin <pearlisrich@naver.com>
2022-06-07 11:46:20 +0900

    set

---
+++ Set.playground/Contents.swift
@@ -0,0 +1,18 @@
+/*
+ 7. 집단 자료형 집합 - 중복된 요소가 제거됨
+ */
+
+var set: Set = [1, 2, 3, 4, 5, 6, 6, 3, 3, 7, 7]
+
+print(set)
+
+var two: Set = [2,2,2,2,2,4,6,8,10,12,14]
+var three: Set = [3,6,9,12,15,18,21]
+
+let result = two.intersection(three)
+
+print(result)
+
+two.union(three)
+two.subtract(three)
+two.symmetricDifference(three)
