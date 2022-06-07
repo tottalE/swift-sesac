6a315472c2fb4f127bcce7aec12853422ef73356
jin <pearlisrich@naver.com>
2022-06-07 18:42:29 +0900

    for in

---
+++ Loop.playground/Contents.swift
@@ -0,0 +1,18 @@
+let musicList = ["아이유 - 좋은날", "이무진 - 과제곡", "BTS - Dynamite"]
+
+for index in 0..<musicList.count {
+    print("음악차트 Top\(index + 1): \(musicList[index])")
+    
+    print("=====================")
+}
+
+for music in 1...3 {
+    print("\(music)위 \(musicList[music - 1])")
+}
+musicList.forEach { music in
+    print(music)
+}
+
+for number in 1...100 {
+    print(number)
+}
